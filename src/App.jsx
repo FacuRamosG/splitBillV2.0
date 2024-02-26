28aa8924422ff436a287a989f1f6052350e9775e
facuramosg <facundoramosgarde@gmail.com>
2024-02-26 16:49:15 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -164,7 +164,7 @@ function App() {
             onChange={handleChange} />
           <button className='text-gray-900 px-3 py-2.5 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-sm  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700' >Sumar participante</button>
           {/* arreglar boton */}
-          {!cargaDeParticipantes && <button onClick={finalizarCarga} className='border p-3 rounded-lg' >Finalizar</button>}
+          {!cargaDeParticipantes && <button onClick={finalizarCarga} className='border px-3 py-2.5 rounded-lg text-sm' >Finalizar</button>}
             
         </form>
         {integrants.length > 0 && !cargaDeParticipantes && 
